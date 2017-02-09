a86a2c92eb8239d391ea33cd381a8384665d3615
rngobile <rngobile>
2017-02-08 23:09:53 -0500

    modified fixed xor

--- fixed_xor.py
+++ fixed_xor.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import binascii
 
+def sxor(s1,s2):
+    return ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(s1,s2))
+
 def main():
     string1 = '1c0111001f010100061a024b53535009181c'
     string2 = '686974207468652062756c6c277320657965'
@@ -10,9 +13,9 @@ def main():
     hex2 = binascii.hexlify(string2)
     resultHex = binascii.unhexlify(resultString)
 
-    result = [ord(a) ^ ord(b) for a,b in zip(hex1,hex2)]
+    finalString = sxor(hex1,hex2)
 
-    print(binascii.hexlify(result))
+    print(finalString)
     print(resultString)
 
 if __name__ == '__main__':main()
