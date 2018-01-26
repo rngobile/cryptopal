12176938439055e91687fb3d9b6a5da2991ff2eb
Richard Ngo <rngobile@gmail.com>
2018-01-26 00:04:25 -0500

    fix indentation

--- go/s01c01.go
+++ go/s01c01.go
@@ -1,5 +1,5 @@
 /*********************************************************************************************************************
-* Cryptopals: 	Set 01 Challenge 01
+* Cryptopals:	Set 01 Challenge 01
 * Title: 		Convert hex to base64
 * Input: 		49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 * Output: 		SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
